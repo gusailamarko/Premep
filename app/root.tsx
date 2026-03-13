9a5f2b199e67eef0d6bb8d6296ded8575344bb39
Gusaila Márkó <gusailamarko10@gmail.com>
2026-03-13 21:15:36 +0100

    Vercel analytics

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+
+import { Analytics } from "@vercel/analytics/react";
 import type { Route } from "./+types/root";
 import "./app.css";
 
@@ -36,6 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <Analytics />
       </body>
     </html>
   );
